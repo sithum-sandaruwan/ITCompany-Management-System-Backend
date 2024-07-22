fbef7f3a8e56f5ceea379d691e5917e057a11a6f
Sithum <sithumsandaruwan200123@gmail.com>
2024-07-22 15:03:07 +0530

    Initial Commit

--- src/main/java/edu/icet/empmanage/Main.java
+++ src/main/java/edu/icet/empmanage/Main.java
@@ -1,7 +1,12 @@
 package edu.icet.empmanage;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(Main.class);
     }
 }
